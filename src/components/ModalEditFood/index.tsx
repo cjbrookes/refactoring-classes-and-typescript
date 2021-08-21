607fdcd3d8842cd4bddf3f9392360c594cb90080
Chris Brookes <christopher@dynamox.net>
2021-08-21 11:32:09 -0300

    modal edit food

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -1,30 +1,38 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 
-class ModalEditFood extends Component {
-  constructor(props) {
-    super(props);
-
-    this.formRef = createRef()
+interface FoodProps {
+  id: number
+  name: string
+  price: string
+  description: string
+  image: string
+  available: boolean
   }
 
-  handleSubmit = async (data) => {
-    const { setIsOpen, handleUpdateFood } = this.props;
+interface ModalEditFoodProps{
+    isOpen: boolean
+    handleUpdateFood: (data:FoodProps) => void
+    editingFood: FoodProps
+    setIsOpen: () => void
+  }  
+
+function ModalEditFood ({isOpen, handleUpdateFood, editingFood, setIsOpen}:ModalEditFoodProps){
+  const formRef = useRef(null)
+
+  const handleSubmit = async (data:FoodProps) => {
 
     handleUpdateFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen, editingFood } = this.props;
-
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
           <h1>Editar Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -42,7 +50,6 @@ class ModalEditFood extends Component {
         </Form>
       </Modal>
     );
-  }
 };
 
 export default ModalEditFood;
